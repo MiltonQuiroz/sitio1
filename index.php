f77be8091c207cdfba5b75336227fb73af96ac7d
root <root@Debian.debian.net>
2021-03-30 09:37:44 -0600

    ADD: se agrega archivo index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+   <head>
+	<title> Prueba de PHP </title>
+   </head>
+   <body>
+   <?php
+     echo "Este mensaje se imprime con PHP";
+   ?>
+     <br />
+       <p>Este mensaje se imprime con HTML</p>
+    <br/>
+   <body>
+</html>
+
+
